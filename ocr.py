728eeffa4740a37f6236eb67d524f6e386aae488
MatteoDelfavero <matteo.delfavero@gmail.com>
2019-01-11 07:14:17 +0100

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -16,17 +16,15 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\t
 debug = 1
 
 def log(question, answer, filename):
-    print(filename)
     string = filename + "\nQuestion:\n\n\"" + question + "\"\n\nAnswer: \n\n\"" + answer + "\"\n\n\n"
     if debug:
-        print("debugmode")
         with open('log/log.txt', 'a', encoding='utf-8') as file:
             file.writelines(string)
 
     print(string)
 
 def OCRimage():
-    #         (x,   y,  width,  height)
+    #     (x,   y,  width,  height)
     pos = (185, 720, 955, 200)
     filename = "honfoglalo " + strftime("%Y-%m-%d %H_%M_%S", gmtime()) + ".jpg"
     return pytesseract.image_to_string(pyautogui.screenshot(imageFilename="log/" + filename, region=(pos))), filename
@@ -34,7 +32,7 @@ def OCRimage():
 def main(args):
     keyword = args[0]
     filename = args[1]
-    google_search = "https://www.google.co.in/search?client=ubuntu&channel=fs&biw=748&bih=875&source=hp&ei=Ugc4XOStLc7CwQK4jbmACg&q=" + keyword
+    google_search = "https://www.google.co.in/search?q=" + keyword
     url = BeautifulSoup(requests.get(google_search).text, "html.parser").findAll('span', {"class":"st"})
 
     for u in url:
